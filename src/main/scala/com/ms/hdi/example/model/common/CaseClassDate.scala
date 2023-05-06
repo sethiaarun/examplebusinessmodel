7634d7322dcb1720743e8ac2b41f73150f7b20d6
arunsethia <arunsethia@microsoft.com>
2023-05-05 19:44:43 -0500

    added date model class

---
+++ src/main/scala/com/ms/hdi/example/model/common/CaseClassDate.scala
@@ -0,0 +1,11 @@
+package com.ms.hdi.example.model.common
+
+import com.ms.hdi.example.model.BaseDataGen
+
+/**
+ * Date
+ *
+ * @param date
+ * @param format
+ */
+case class CaseClassDate(date: String, format: String) extends BaseDataGen
