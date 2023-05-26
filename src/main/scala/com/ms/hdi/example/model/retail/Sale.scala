724edb4358b3c11ef748c4110e09d65ffcc819ad
arunsethia <arunsethia@microsoft.com>
2023-05-26 09:25:03 -0500

    extended sales object

--- src/main/scala/com/ms/hdi/example/model/retail/Sale.scala
+++ src/main/scala/com/ms/hdi/example/model/retail/Sale.scala
@@ -6,6 +6,8 @@ import com.ms.hdi.example.model.BaseDataGen
  * Sales data
  * @param customerId customer id
  * @param date sale date/tx date
+ * @param saleQty sale qty
+ * @param itemId sale Item Id
  * @param saleValue sale value
  */
-case class Sale(customerId:Int, date:String, saleValue: Float) extends BaseDataGen
+case class Sale(customerId:Int, date:String, saleQty: Int, itemId: Int, saleValue: Float) extends BaseDataGen
